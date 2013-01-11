cccbfd20e250dd968351af0554cac85bc4e89f0b
Alex <damaximum@live.com>
2013-01-11 15:33:40 -0500

    these are the temporary launcher files

---
+++ Launcher.h
@@ -0,0 +1,24 @@
+#indef LAUNCHER_H
+#define LAUNCHER_H
+#include <cstdio>
+
+class Launcher{
+    private:
+    const int COUNT_THRESHOLD = 70; //rpm
+    const int ON_SPEED = 100;//rpm
+    const float AT_SPEED_TOLERANCE = 2; //rpm
+    unsigned int count = 0;
+    float GetSpeed();
+    public:
+    launcher();
+    ~launcher();
+    void start();
+    void stop();
+    void update();
+    bool atSpeed();
+    unsigned int getFrisbeeCount();
+    void resetFrisbeeCount();
+
+};
+
+#endif
