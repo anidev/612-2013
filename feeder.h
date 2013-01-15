6f27ffe154d870949f707b5f781e91e566b01c8c
swarage <sdcoolio@gmail.com>
2013-01-15 15:44:34 -0500

    added more stuff to feeder

--- feeder.h
+++ feeder.h
@@ -3,9 +3,14 @@
 
 class Feeder {
 private:
-
+    int direction = 0; //change to uint32 later if needed
+    void set_direction(int);
 public:
-    void load_frisbee(double button_index);	
+    Feeder();
+    ~Feeder();
+    void feeder_up();
+    void feeder_down();
+    void feeder_stop_move();
 };
 
 #endif // FEEDER_H
