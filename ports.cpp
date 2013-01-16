4b7076629efd34729de2d6c42683bc07bffeb8b7
anidev <anidev.aelico@gmail.com>
2013-01-16 17:15:45 -0500

    Added jaguar information

--- ports.cpp
+++ ports.cpp
@@ -1,7 +1,16 @@
 #include <Joystick.h>
+#include <Jaguar.h>
+#include <RobotDrive.h>
 #include "ports.h"
 #include "Launcher.h"
 
 Joystick left_joystick  (1);
 Joystick right_joystick (2);
 Joystick gunner_joystick(3);
+
+Jaguar left_front_jag(left_front_motor.moduleNumber,left_front_motor.channel);
+Jaguar left_rear_jag(left_rear_motor.moduleNumber,left_rear_motor.channel);
+Jaguar right_front_jag(right_front_motor.moduleNumber,right_front_motor.channel);
+Jaguar right_rear_jag(right_rear_motor.moduleNumber,right_rear_motor.channel);
+
+RobotDrive drive_train(left_front_jag,left_rear_jag,right_front_jag,right_rear_jag);
--- ports.h
+++ ports.h
@@ -17,4 +17,9 @@ extern Joystick right_joystick;
 extern Joystick gunner_joystick;
 extern RobotDrive drive_train;
 
+static const hw_info left_front_motor={2,2};
+static const hw_info left_rear_motor={2,3};
+static const hw_info right_front_motor={1,3};
+static const hw_info right_rear_motor={1,4};
+
 #endif
