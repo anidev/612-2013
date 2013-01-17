c209d85b86040e2c5ae984939d368d57edaeafaf
swarage <sdcoolio@gmail.com>
2013-01-17 16:28:16 -0500

    added to state_drive.cpp

--- states/state_driving.cpp
+++ states/state_driving.cpp
@@ -5,5 +5,9 @@
 void driving_state() {
     float left_axis = left_joystick.GetY ();
     float right_axis = right_joystick.GetY ();
-    drive_train.TankDrive (left_axis, right_axis);
+    if (right_joystick.GetRawButton(1)) {
+        drive_train.ArcadeDrive (right_joystick);
+    } else {
+        drive_train.TankDrive(left_axis, right_axis);
+    }
 }
