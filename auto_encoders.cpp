6550c280d37821590b6eae9c86ce9a5f91ffa82d
anidev <anidev.aelico@gmail.com>
2013-01-19 12:06:41 -0500

    Minor syntax and structure changes for auto_encoders

--- auto_encoders.cpp
+++ auto_encoders.cpp
@@ -4,19 +4,23 @@ auto_encoders::auto_encoders(hw_info left1, hw_info right1, hw_info left2, hw_in
     left_encoder = new Encoder(left1.moduleNumber, left1.channel, left2.moduleNumber, left2.channel);
     right_encoder = new Encoder(right1.moduleNumber, right1.channel, right2.moduleNumber, right2.channel);
 }
-double auto_encoders::calc_distance() {
-    return (left_encoder.Get() + right_encoder.Get()) / 2;
+double auto_encoders::get_avg_ticks() {
+    return (left_encoder->Get() + right_encoder->Get()) / 2;
 }
-double auto_encoders::convert_distance(double ticks) {
+double auto_encoders::convert_distance(int ticks) {
     //this finds distance in feet
-    return (ticks/TICKS_PER_REV)*(WHEEL_RADIUS*(pi))/12;
+    return (ticks*1.0/TICKS_PER_REV)*(WHEEL_RADIUS*(pi))/12.0;
 }
-double auto_encoders::get_distance() {
+double auto_encoders::get_target_distance() {
     return distance;
 }
+double auto_encoders::get_distance() {
+    int ticks=get_avg_ticks();
+    return convert_distance(ticks);
+}
 void auto_encoders::reset_distance(){
-    left_encoder.Reset();
-    right_encoder.Reset();
+    left_encoder->Reset();
+    right_encoder->Reset();
 }    
 void auto_encoders::start_driving(double dist){
     is_driving = true;
