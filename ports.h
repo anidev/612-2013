a6111c0aca7bad0aaee850267a5405a2944e3bb0
JeffTolbert <cobras7@verizon.net>
2013-01-24 16:11:33 -0500

    Formatting Changes

--- ports.h
+++ ports.h
@@ -16,14 +16,14 @@ extern Joystick right_joystick;
 extern Joystick gunner_joystick;
 extern RobotDrive drive_train;
 extern auto_encoders encoders;
-
-static const hw_info left_front_motor={2,2};
-static const hw_info left_rear_motor={2,3};
-static const hw_info right_front_motor={1,3};
-static const hw_info right_rear_motor={1,4};
+//Drive Motors
+static const hw_info left_front_motor  = {2,2};
+static const hw_info left_rear_motor   = {2,3};
+static const hw_info right_front_motor = {1,3};
+static const hw_info right_rear_motor  = {1,4};
+//Encoders
 static const hw_info right_encoder1 = {1, 1};
 static const hw_info right_encoder2 = {1, 2};
-static const hw_info left_encoder1 = {2, 4};
-static const hw_info left_encoder2 = {2, 5};
-
+static const hw_info left_encoder1  = {2, 4};
+static const hw_info left_encoder2  = {2, 5};
 #endif
