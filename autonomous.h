9ab90849a4134cba7dc83aef9c5053ea2d9db6ee
swarage <sdcoolio@gmail.com>
2013-01-26 13:19:40 -0500

    made autonomous functions. I am not sure as to how they will be run at the moment. We also need to address the problem of shooting in autonomous

--- autonomous.h
+++ autonomous.h
@@ -5,8 +5,13 @@
 #include <Encoder.h>
 #include "ports.h"
 
-void choose_routine(/*add parameter corresponding to pressed button*/);
+void choose_routine(int,int);
 void drive();
 void turn();
+//subroutines
+void front_left(int);
+void front_right(int);
+void back_left(int);
+void back_right(int);
 
 #endif
