3be6d910ac2702e8a4566136dfc0d4aedfd1c93b
swarage <sdcoolio@gmail.com>
2013-01-25 20:58:25 -0500

    added more specific functions to auto_encoders.

--- auto_encoders.cpp
+++ auto_encoders.cpp
@@ -32,16 +32,44 @@ double auto_encoders::get_distance() {
 //    std::printf("ticks avg: %f\n",get_avg_ticks());
 //    return 0.0;
 }
+double auto_encoders::get_left_dist() {
+    return left_encoder->GetDistance();
+}
+double auto_encoders::get_right_dist() {
+    return right_encoder->GetDistance();
+}
+double auto_encoders::get_left_target_dist() {
+    return left_distance; 
+}
+double auto_encoders::get_right_target_dist() {
+    return right_distance;
+}
+void auto_encoders::start_left_driving(double dist) {
+   left_distance = dist; 
+}
+void auto_encoders::start_right_driving(double dist) {
+    right_distance = dist;
+}
+void reset_left_distance() {
+    left_encoder->Reset();
+}
+void reset_right_distance() {
+    right_encoder->Reset();
+}
+bool left_at_target() {
+    return ((std::fabs(left_distance) - get_left_dist)) < 1.5);
+}
+bool right_at_target() {
+    return ((std::fabs(right_distance) - get_right_dist)) < 1.5);
+}
 void auto_encoders::reset_distance(){
     left_encoder->Reset();
     right_encoder->Reset();
 }
-/*
 void auto_encoders::start_driving(double dist){
     is_driving = true;
     distance = dist;
 }
-*/
 void auto_encoders::stop_driving() {
     distance = 0;
     is_driving = false;
