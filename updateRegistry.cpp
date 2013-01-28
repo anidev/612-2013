0f02af01d25a4134a1a7aba0bbefb05f39f20785
JeffTolbert <cobras7@verizon.net>
2013-01-28 13:18:22 -0500

    Removed print statements

--- updateRegistry.cpp
+++ updateRegistry.cpp
@@ -2,20 +2,16 @@
 #include "UpdateRegistry.h"
 
 UpdateRegistry::UpdateRegistry() {
-    std::printf("Registry Created");
 }
 UpdateRegistry::~UpdateRegistry() {
 }
 void UpdateRegistry::addUpdateFunction(functionName f,inst i) {
     instances.push_back(i);
     functions.push_back(f);
-    std::printf("Function added!\n");
 }
 void UpdateRegistry::updateAll() {
-    std::printf("Size: %i \n", instances.size());
     for(unsigned int x = 0; x < instances.size(); x++)
     {
         (functions.at(x))(instances.at(x));
-        std::printf("function called\n");
     }
 }
