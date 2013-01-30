7af56d3a135131f417cb00a097505785fc930f28
anidev <anidev.aelico@gmail.com>
2013-01-30 15:57:48 -0500

    Initialized in ports.cpp and organized in ports.h

--- ports.cpp
+++ ports.cpp
@@ -4,7 +4,9 @@
 #include "ports.h"
 #include "launcher.h"
 #include "auto_encoders.h"
+#include "drivetrain.h"
 #include "shifter.h"
+#include "lift.h"
 #include "612.h"
 
 UpdateRegistry updateRegistry; // Must be first constructed
@@ -12,4 +14,11 @@ EnhancedJoystick left_joystick   (1);
 EnhancedJoystick right_joystick  (2);
 EnhancedJoystick gunner_joystick (3);
 
+drivetrain_info dinfo={left_front_motor,left_rear_motor,right_front_motor,right_rear_motor};
+encoders_info einfo={left_encoder1,left_encoder2,right_encoder1,right_encoder2};
+DriveTrain drive_train(dinfo,einfo);
+
 shifter shift(side1,side2);
+
+Lift angleAdjuster(lift_motor);
+
