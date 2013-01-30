43e4888320f4398d3a45ea147773f8dee6d129f2
swarage <sdcoolio@gmail.com>
2013-01-30 16:01:05 -0500

    we improved the autonomous code

--- autonomous.cpp
+++ autonomous.cpp
@@ -1,40 +1,32 @@
 #include "autonomous.h"
 #include "ports.h"
+#include "shooterAim.h"
+#include "state.h"
+#include "drivetrain.h"
+#include <cmath>
 
-void choose_routine() {
-    
-}
+bool isLeft;
+int Frisbees = 3;
+const int DRIVE_DIST=119;
+const int TURN_ANGLE= -29; // negative is clockwise, positive is counter-clockwise
+enum auto_states {
+	AUTO_DRIVE,
+	AUTO_TURN,
+	AUTO_AIM,
+	AUTO_SHOOT
+};
+
+State state(AUTO_DRIVE);
 
 void drive(double dist /*keep in mind that dist is in inches*/) {
-/*    encoders.reset_distance();
-    while (encoders.get_distance() < dist) {
-        drive_train.TankDrive(1,1); //change the (1,1) to something safer later
-    }
-    encoders.stop_driving();*/
+	drive_train.drive(dist);
 }
 
 void turn(double angle) {
-    /*
-    keep in mind that the angles in use will be calculated with the robot
-    facing the 0deg mark. This means that turning to the right will require a negative angle
-    and turning to the left will require a positive angle. 
-    if the angle is negative, then the motors on the right side of the robot
-    will need to be positively powered and the left motors will need to be negatively powered and 
-    vice versa if the angle is positive.
-    note: get width of robot
-    note: this method should be able to calculate the change in angle 
-    */
-/*    double distance = angle / 3.07912798856;
-    encoders.reset_distance();
-    encoders.stop_driving();
-    if (angle > 0) {
-        while (encoders.get_distance() < distance) {
-            drive_train.TankDrive(-1,1); //change the (1,1) to something safer later
-        }
-    } else if (angle < 0) {
-        while (encoders.get_distance() < distance) {
-            drive_train.TankDrive(1,-1); //change the (1,1) to something safer later
-        }
-    }*/
+    drive_train.turn(angle);
 }
 
+
+/*This code is for shooting at the high goal, implement this when finished with mid goal     */
+
+/* THIS CODE BELONGS TO ZACK, PRESUME IT DOESN'T WORK*/ /*oh ya and adrian and swaraj*/
--- autonomous.h
+++ autonomous.h
@@ -2,10 +2,16 @@
 #define AUTONOMOUS_H
 
 #include "auto_encoders.h"
+#include "drivetrain.h"
 #include <Encoder.h>
 #include "ports.h"
 
-void choose_routine(/*add parameter corresponding to pressed button*/);
+enum Position {Back_Left, Back_Right, Front_Left, Front_Right}; 
+enum Target {Middle_Goal, High_Goal};
+
+void choose_routine(Position, Target);
+void do_autonomous();
+
 void drive();
 void turn();
 
