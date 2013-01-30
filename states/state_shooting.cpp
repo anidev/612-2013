e1240451d58f7affda5ef460f99373a9ba0ff1cf
alonBendelac <alon.bendelac@gmail.com>
2013-01-30 15:17:59 -0500

    Made functions for joystick buttons.

--- states/state_shooting.cpp
+++ states/state_shooting.cpp
@@ -1,10 +1,22 @@
 #include "state_climbing.h"
 #include "state_shooting.h"
+#include <Joystick.h>           // for GetRawButton ()
 
 void shooting_auto() {
     // TODO
 }
 
 void shooting_manual() {
-    // TODO
+    if (GetRawButton (1)) {     // X
+        // unreadable in pdf file
+    }
+    if (GetRawButton (4)) {     // Y
+        auto_shoot ();
+    }
+    if (GetRawButton ()) {      // angle up
+        lift_up();
+    }
+    if (GetRawButton ()) {      // angle down
+        lift_down();
+    }
 }
