8ac8a3fb32c0ff020cff88f625b1a0175d34d6d2
anidev <anidev.aelico@gmail.com>
2013-01-29 19:04:21 -0500

    Removed unnecessary shift.update() from driving state

--- states/state_driving.cpp
+++ states/state_driving.cpp
@@ -1,20 +1,16 @@
-#include <RobotDrive.h>
 #include <Timer.h>
 #include "../ports.h"
 #include "../auto_encoders.h"
-#include "state_driving.h"
 #include "../shifter.h"
+#include "../drivetrain.h"
+#include "state_driving.h"
 
 void driving_state() {
-    float left_axis = left_joystick.GetY ();
-    float right_axis = right_joystick.GetY ();
-    if (left_joystick.GetRawButton(6)) {
-        encoders.reset_distance();
-    }
     if (left_joystick.GetRawButton(1)) {
         drive_train.ArcadeDrive(left_joystick);
     } else {
+        float left_axis = left_joystick.GetY ();
+        float right_axis = right_joystick.GetY ();
         drive_train.TankDrive(left_axis, right_axis);
     }
-    shift.update();
 }
