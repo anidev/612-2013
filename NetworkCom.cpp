88119f84c76f9bab865e747a3eed00b85c1844f4
anidev <anidev.aelico@gmail.com>
2013-02-09 09:10:59 -0500

    Fixed NetworkCom names

--- NetworkCom.cpp
+++ NetworkCom.cpp
@@ -18,23 +18,23 @@ void NetworkCom::target_available (bool target){
 	Tracking->PutBoolean ("Available", target);
 }
 void NetworkCom::target_x (double new_target){
-	Tracking->PutNumber("Target_x", new_target);
+	Tracking->PutNumber("X", new_target);
 }
 void NetworkCom::target_y (double new_target){
-	Tracking->PutNumber("Target_y", new_target);
+	Tracking->PutNumber("Y", new_target);
 }
 void NetworkCom::target_width (double new_target){
-	Tracking->PutNumber("Target_width", new_target);
+	Tracking->PutNumber("Width", new_target);
 }
 void NetworkCom::target_height (double new_target){
-	Tracking->PutNumber("Target_height", new_target);
+	Tracking->PutNumber("Height", new_target);
 }
 void NetworkCom::launcher_target_speed (double new_speed){
-	Gunner->PutNumber("Launcher_target_speed", new_speed);
+	Gunner->PutNumber("LauncherTarget", new_speed);
 }
 void NetworkCom::launcher_current_speed (double cur_speed){
-	Gunner->PutNumber("Launcher_current_speed", cur_speed);
+	Gunner->PutNumber("LauncherCurrent", cur_speed);
 }
 void NetworkCom::lift_angle (float new_angle){
-	Gunner->PutNumber("Lift_angle", new_angle);
+	Gunner->PutNumber("LiftAngle", new_angle);
 }
