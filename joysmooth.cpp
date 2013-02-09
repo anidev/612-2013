525fb7bf3291fe40cd76e2a5d65b348264f71c53
JeffTolbert <cobras7@verizon.net>
2013-02-08 23:18:35 -0500

    Merge Plus I changed Joysmooth

--- joysmooth.cpp
+++ joysmooth.cpp
@@ -1,13 +1,10 @@
 #include <Joystick.h>
 #include <bitset>
+#include "612.h"
 #include "joysmooth.h"
 
-//defines the constants from .h
-const int joysmooth::NUMBUTTONS;
-const int joysmooth::WAIT_TIME;
-int true_filter;
-
-joysmooth::joysmooth(UINT32 port):real_joy(port) {
+joysmooth::joysmooth(UINT32 port):Joystick(port) {
+    updateRegistry.addUpdateFunction(&updateHelper,(void*)this);
 }
 
 joysmooth::~joysmooth () {
@@ -18,65 +15,17 @@ void joysmooth::update(){
         for (int s = 1; s < WAIT_TIME; s++){
             buttons[f][s - 1] = buttons [f][s];
         }
-        buttons[f][WAIT_TIME - 1] = real_joy.GetRawButton(f);      //me no leik pointers, swarage fix dis
+        buttons[f][WAIT_TIME - 1] = GetRawButton(f);
     }
 }
-
-float joysmooth::GetX(JoystickHand hand){
-    return real_joy.GetX(hand);
-}
-
-float joysmooth::GetY(JoystickHand hand){
-    return real_joy.GetY(hand);
-}
-
-float joysmooth::GetZ(){
-    return real_joy.GetZ();
-}
-
-float joysmooth::GetTwist () {
-    return real_joy.GetTwist();
-}
-float joysmooth::GetThrottle () {
-    return real_joy.GetThrottle();
-}
-float joysmooth::GetRawAxis (UINT32 axis) {
-    return real_joy.GetRawAxis(axis);
-}
-bool joysmooth::GetTrigger (JoystickHand hand) {
-    return real_joy.GetTrigger();
-}
-bool joysmooth::GetTop (JoystickHand hand) {
-    return real_joy.GetTop();
-}
-bool joysmooth::GetBumper (JoystickHand hand) {
-    return real_joy.GetBumper();
-}
-bool joysmooth::GetRawButton (UINT32 button) {
-    return real_joy.GetRawButton(button);
-}
 bool joysmooth::GetButton(UINT32 btn) {
-    //Todo filter Btn Inputs
    for (int i = 0; i < WAIT_TIME; i++){
        if (!buttons[btn][i]){
            return false;
        }
-
    }
    return true;
-
-    /*
-    int true_filter = 0;
-    for (int i = 0; i < 3; i++ ){
-        if (real_joy.GetRawButton(btn)){
-            true_filter += 1;
-        }
-    }
-    if (true_filter == 3){
-        return true;
-    }
-    return false;
-    */
-
-    // return GetRawButton(btn);
+}
+void joysmooth::updateHelper(void* o) {
+    ((joysmooth*)o) -> update();
 }
--- joysmooth.h
+++ joysmooth.h
@@ -4,27 +4,17 @@
 #include <Joystick.h>
 #include <bitset>
 
-class joysmooth : public GenericHID {
+class joysmooth : public Joystick {
 private:
-    Joystick real_joy;
     const static int NUMBUTTONS = 11;
-    const static int WAIT_TIME = 3; //total amount of clicks to wait for
-    std::bitset<3> buttons[NUMBUTTONS];
+    const static int WAIT_TIME = 3; //total amount of checks
+    std::bitset<WAIT_TIME> buttons[NUMBUTTONS];
 public:
     joysmooth (UINT32 port);
     ~joysmooth ();
-    float GetX (JoystickHand hand = kRightHand);
-    float GetY (JoystickHand hand = kRightHand);
-    float GetZ ();
-    float GetTwist ();
-    float GetThrottle ();
-    float GetRawAxis (UINT32 axis);
-    bool GetTrigger (JoystickHand hand = kRightHand);
-    bool GetTop (JoystickHand hand = kRightHand);
-    bool GetBumper (JoystickHand hand = kRightHand);
-    bool GetRawButton (UINT32);
     bool GetButton (UINT32);
     void update();
+    static void updateHelper(void*);
 };
 
 #endif
