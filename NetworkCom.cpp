60a1dca2c8846b321c1839b9c152a0eb0803f7d7
unknown <damaximum@live.com>
2013-02-12 15:45:16 -0500

    adding stuff

--- NetworkCom.cpp
+++ NetworkCom.cpp
@@ -13,44 +13,67 @@ NetworkCom::NetworkCom() {
 
 NetworkCom::~NetworkCom() {
 }
-
+//if null end function
 void NetworkCom::target_available (bool target){
-    if (target != Tracking->GetBoolean("Available")) {
-    	Tracking->PutBoolean ("Available", target);
-    }
+	if(Tracking != null){
+	    if (target != Tracking->GetBoolean("Available")) {
+	    	Tracking->PutBoolean ("Available", target);
+	    }
+	}
 }
 void NetworkCom::target_x (double new_target){
-    if (new_target != Tracking->GetNumber("X")) {
-    	Tracking->PutNumber("X", new_target);
-    }
+	if(Tracking != null){
+		if (new_target != Tracking->GetNumber("X")) {
+		    	Tracking->PutNumber("X", new_target);
+		}
+	}
+    
 }
 void NetworkCom::target_y (double new_target){
-    if (new_target != Tracking->GetNumber("Y")) {
-    	Tracking->PutNumber("Y", new_target);
+    if(Tracking != null){
+    	if (new_target != Tracking->GetNumber("Y")) {
+    	    	Tracking->PutNumber("Y", new_target);
+    	}
     }
+	
 }
 void NetworkCom::target_width (double new_target){
-    if (new_target != Tracking->GetNumber("Width")) {
-    	Tracking->PutNumber("Width", new_target);
-    }
+	if(Tracking != null){
+		if (new_target != Tracking->GetNumber("Width")) {
+		    	Tracking->PutNumber("Width", new_target);
+		}	
+	}
+    
 }
 void NetworkCom::target_height (double new_target){
-    if (new_target != Tracking->GetNumber("Height")) {
-    	Tracking->PutNumber("Height", new_target);
-    }
+	if(Tracking != null){
+		if (new_target != Tracking->GetNumber("Height")) {
+		    	Tracking->PutNumber("Height", new_target);
+		}
+	}
+    
 }
 void NetworkCom::launcher_target_speed (double new_speed){
-    if (new_speed != Gunner->GetNumber("LauncherTarget")) {
-    	Gunner->PutNumber("LauncherTarget", new_speed);
-    }
+	if(Tracking != null){
+		if (new_speed != Gunner->GetNumber("LauncherTarget")) {
+		    	Gunner->PutNumber("LauncherTarget", new_speed);
+		}
+	}
+    
 }
 void NetworkCom::launcher_current_speed (double cur_speed){
-    if (cur_speed != Gunner->GetNumber("LauncherCurrent")) {
-    	Gunner->PutNumber("LauncherCurrent", cur_speed);
-    }
+	if(Tracking !=null){
+		if (cur_speed != Gunner->GetNumber("LauncherCurrent")) {
+		    	Gunner->PutNumber("LauncherCurrent", cur_speed);
+		}
+	}
+    
 }
 void NetworkCom::lift_angle (float new_angle){
-    if (new_angle != Gunner->GetNumber("LiftAngle")) {
-    	Gunner->PutNumber("LiftAngle", new_angle);
-    }
+	if(Tracking !=null){
+		if (new_angle != Gunner->GetNumber("LiftAngle")) {
+		    	Gunner->PutNumber("LiftAngle", new_angle);
+		}
+	}
+    
 }
