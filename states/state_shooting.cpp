0d953523029b9e1c5f6e61a7375757e49d4aaff4
anidev <anidev.aelico@gmail.com>
2013-02-12 22:39:20 -0500

    Added more debug statements to state shooting and twocontroller...

--- states/state_shooting.cpp
+++ states/state_shooting.cpp
@@ -32,7 +32,7 @@ void shooting_manual() {
         angleAdjuster.lift_down();
     }
     else if(gunner_joystick.GetRawAxis (6) < -0.98f) {  // angle up
-        std::printf("angle up\n");
+        std::printf("angle up %f\n",angleAdjuster.get_current_angle());
         angleAdjuster.lift_up();
     }
     else
@@ -42,10 +42,11 @@ void shooting_manual() {
 
     if (gunner_joystick.GetRawButton(1)) {
         if(!speed_set) {
-            std::printf("launcher set%f\n",angleAdjuster.get_current_angle());
+            std::printf("launcher set\n");
             shooter.setSpeed (new_shooter_wheel_speed);
             speed_set=true;
         }
+        std::printf("launcher speed: %f\n",shooter.getCurrentSpeed());
     }
     else {
         if(speed_set) {
@@ -57,12 +58,12 @@ void shooting_manual() {
 
     if (gunner_joystick.GetRawButton (5)) {             // slow down shooter wheel
         // save new speed, change speed when buton is not pressed
-        std::printf("shooter slowed to %f\n",new_shooter_wheel_speed);
+        std::printf("launcher slowed to %f\n",new_shooter_wheel_speed);
         new_shooter_wheel_speed -= 0.2;
     }
 
     if (gunner_joystick.GetRawButton (6)) {             // speed up shooter wheel
-        std::printf("shooter sped up to %f\n",new_shooter_wheel_speed);
+        std::printf("launcher sped up to %f\n",new_shooter_wheel_speed);
         new_shooter_wheel_speed += 0.2;
     }
 
