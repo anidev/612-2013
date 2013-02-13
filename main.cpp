656c84edf11415d770eec61c90b87522b0c5a8b2
anidev <anidev.aelico@gmail.com>
2013-02-13 17:03:38 -0500

    Checks driver using joysticks, autonomous target selection, removed wait

--- main.cpp
+++ main.cpp
@@ -9,7 +9,25 @@
 #include "ports.h"
 #include "auto_encoders.h"
 #include "autonomous.h"
+#include "EnhancedJoystick.h"
 #include "main.h"
+
+const float JOY_THRESHOLD=0.05;
+void driver_check_update(void* dummy) {
+    if(std::fabs(right_joystick.GetY())>JOY_THRESHOLD) {
+        driverOperation=true;
+        return;
+    }
+    if(std::fabs(left_joystick.GetY())>JOY_THRESHOLD) {
+        driverOperation=true;
+        return;
+    }
+    if(left_joystick.GetRawButton(1)&&std::fabs(left_joystick.GetX())>JOY_THRESHOLD) {
+        driverOperation=true;
+        return;
+    }
+    driverOperation=false;
+}
 
 robot_class::robot_class() {
     GetWatchdog().SetEnabled(false);
@@ -18,6 +36,7 @@ robot_class::robot_class() {
 void robot_class::RobotInit() {
     std::printf("RobotInit");
     main_table=NetworkTable::GetTable("612");
+    updateRegistry.addUpdateFunction(&driver_check_update,NULL);
 }
 
 void robot_class::DisabledInit() {
@@ -31,10 +50,12 @@ void robot_class::AutonomousInit() {
     // 2 = Front Left
     // 3 = Back Right
     Position auto_pos=Back_Left;
+    AutoTarget auto_target=Middle_Goal;
     if(main_table!=NULL) {
         NetworkTable* auto_table=main_table->GetSubTable("Autonomous");
         if(auto_table!=NULL) {
             int positionVal=(int)auto_table->GetNumber("Position");
+            int targetVal=(int)auto_table->GetNumber("Target");
             switch(positionVal) {
             case 0:
                 auto_pos=Front_Right;
@@ -53,9 +74,19 @@ void robot_class::AutonomousInit() {
                 std::printf("Selected autonomous: BACK RIGHT\n");
                 break;
             }
+            switch(targetVal) {
+            case 0:
+                auto_target=High_Goal;
+                std::printf("Selected autonomous: HIGH GOAL\n");
+                break;
+            case 1:
+                auto_target=Middle_Goal;
+                std::printf("Selected autonomous: MID GOAL\n");
+                break;
+            }
         }
     }
-    choose_routine(auto_pos, High_Goal);
+    choose_routine(auto_pos, auto_target);
 }
 
 void robot_class::TeleopInit() {
@@ -73,7 +104,12 @@ void robot_class::AutonomousPeriodic() {
 }
 
 void robot_class::TeleopPeriodic() {
+    static int counter=0;
     updateRegistry.updateAll();
+    if(counter%20==0) {
+        std::printf("Driver operating: %s\n",(driverOperation?"TRUE":"FALSE"));
+    }
+    counter++;
     if(global_state.get_state()==DRIVE) {
         driving_state();
         shooting_manual(); // at same time as driving
@@ -84,7 +120,6 @@ void robot_class::TeleopPeriodic() {
     else if(global_state.get_state() == SHOOT_AUTO) {
         shooting_auto();
     }
-    Wait(0.015); // let the CPU rest a bit
 }
 
 //the following macro tells the library that we want to generate code
