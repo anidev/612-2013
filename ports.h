9a68a42efcb5b5cf79e3fd22462df3728551efba
unknown <damaximum@live.com>
2013-02-15 21:18:49 -0500

    Netcom changes

--- ports.h
+++ ports.h
@@ -11,6 +11,7 @@ class Lift;
 class Shooter;
 class EnhancedJoystick;
 class AutoShooter;
+class NetworkCom;
 
 typedef int canport_t;
 
@@ -26,6 +27,7 @@ extern DriveTrain drive_train;
 extern Lift angleAdjuster;
 extern Shooter shooter;
 extern AutoShooter auto_shoot;
+extern NetworkCom netcom;
 //lifter
 static const hw_info launch_angle_pot =  {1, 1}; // suzie;()
 static const hw_info lift_motor =        {2, 1}; // suzie;(2,3) - New Robot
