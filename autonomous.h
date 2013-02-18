4a9345355726405d6414979de11a17934969b114
JeffTolbert <cobras7@verizon.net>
2013-02-18 12:56:29 -0500

    Now it can compile

--- autonomous.h
+++ autonomous.h
@@ -8,11 +8,11 @@
 enum Position {Back_Left, Back_Right, Front_Left, Front_Right}; 
 enum AutoTarget {Middle_Goal, High_Goal};
 
-static void choose_routine(Position, AutoTarget);
-static void do_autonomous();
+void choose_routine(Position, AutoTarget);
+void do_autonomous();
 
-static void drive();
-static void turn();
-static void shoot();
+void drive();
+void turn();
+void shoot();
 
 #endif
