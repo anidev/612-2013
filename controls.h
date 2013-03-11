411c686e6e60046919bf31c54ef831d4e5bc014a
anidev <anidev.aelico@gmail.com>
2013-03-11 10:04:51 -0400

    Renamed buttons.h to controls.h and updated values

---
+++ controls.h
@@ -0,0 +1,36 @@
+#ifndef CONTROLS_H
+#define CONTROLS_H
+
+//DRIVER CONTROLS
+//Axes
+const int Driver_Axis_TankLeft     =  2;
+const int Driver_Axis_TankRight    =  4;
+const int Driver_Axis_Arcade       =  6;
+//Buttons
+const int Driver_Btn_LogSensors    =  1;
+const int Driver_Btn_ToggleLED     =  3;
+const int Driver_Btn_ResetEncoders =  4;
+const int Driver_Btn_ShiftLow      =  5;
+const int Driver_Btn_ShiftHigh     =  6;
+const int Driver_Btn_SwivelLeft    =  7;
+const int Driver_Btn_SwivelRight   =  8;
+const int Driver_Btn_NormalMode    =  9;
+const int Driver_Btn_ClimbMode     = 10;
+
+//GUNNER CONTROLS
+//Axes
+const int Gunner_Axis_Feeder        =  5;
+const int Gunner_Axis_ShooterSpeed  =  6;
+//Buttons
+const int Gunner_Btn_PresetOne      =  1;
+const int Gunner_Btn_SingleShot     =  2;
+const int Gunner_Btn_MultiShot      =  3;
+const int Gunner_Btn_PresetTwo      =  4;
+const int Gunner_Btn_LiftUp         =  5;
+const int Gunner_Btn_LiftDown       =  6;
+const int Gunner_Btn_SwivelLeft     =  7;
+const int Gunner_Btn_SwivelRight    =  8;
+const int Gunner_Btn_ShooterRev     =  9;
+const int Gunner_Btn_LiftLoadPreset = 10;
+
+#endif // CONTROLS_H
