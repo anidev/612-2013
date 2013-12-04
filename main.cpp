5e76451bacd58d79fea8c5281467b1413782d944
anidev <anidev.aelico@gmail.com>
2013-12-04 16:03:05 -0500

    Added shifting to suzie-drive

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include "main.h"
+#include "Controls.h"
+
+const float LOWGEAR=0.8f;
+const float HIGHGEAR=0.2f;
 
 robot_class::robot_class():left_front(2,2),
                            left_rear(2,3),
@@ -16,8 +20,8 @@ robot_class::~robot_class() {
 }
 
 void robot_class::RobotInit() {
-    shift_left.Set(0.8f);
-    shift_right.Set(0.8f);
+    shift_left.Set(LOWGEAR);
+    shift_right.Set(LOWGEAR);
 }
 
 void robot_class::DisabledInit() {
@@ -39,14 +43,28 @@ void robot_class::AutonomousPeriodic() {
 }
 
 void robot_class::TeleopPeriodic() {
+    bool shiftLow=false;
+    bool shiftHigh=false;
 #ifdef JOYSTICK
     float left=controller1.GetY();
     float right=controller2.GetY();
 #else
-    float left=controller1.GetRawAxis(2);
-    float right=controller1.GetRawAxis(4);
+    float left=controller1.GetRawAxis(DRIVER_LEFT_DRIVE_AXIS);
+    float right=controller1.GetRawAxis(DRIVER_RIGHT_DRIVE_AXIS);
+    if(controller1.GetRawButton(DRIVER_SHIFT_LOW)) {
+        shiftLow=true;
+    } else if(controller1.GetRawButton(DRIVER_SHIFT_HIGH)) {
+        shiftHigh=true;
+    }
 #endif
     drive.TankDrive(left,right);
+    if(shiftLow) {
+        shift_left.Set(LOWGEAR);
+        shift_right.Set(LOWGEAR);
+    } else if(shiftHigh) {
+        shift_left.Set(HIGHGEAR);
+        shift_right.Set(HIGHGEAR);
+    }
 }
 
 START_ROBOT_CLASS(robot_class)
